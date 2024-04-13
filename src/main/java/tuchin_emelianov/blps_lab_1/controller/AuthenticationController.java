098017067ae59462b37a73e2ed93f271dd1f09c5
Artem <tuchin12345678artem@gmail.com>
2024-04-13 17:33:48 +0300

    preAuthorize in AuthenticationController

--- src/main/java/tuchin_emelianov/blps_lab_1/controller/AuthenticationController.java
+++ src/main/java/tuchin_emelianov/blps_lab_1/controller/AuthenticationController.java
@@ -6,13 +6,12 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tuchin_emelianov.blps_lab_1.dto.HumanDTO;
 import tuchin_emelianov.blps_lab_1.exceptions.BlankFieldException;
+import tuchin_emelianov.blps_lab_1.exceptions.UserAlreadyExistsException;
 import tuchin_emelianov.blps_lab_1.request.SignUpRequest;
 import tuchin_emelianov.blps_lab_1.service.HumanService;
 
@@ -22,10 +21,17 @@ public class AuthenticationController {
     private final HumanService humanService;
     private final ModelMapper modelMapper;
 
-    @PostMapping("/sign-up")
     @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/sign-up")
+    @PreAuthorize("isAnonymous()")
     public HumanDTO signUp(@RequestBody @Valid SignUpRequest signUpRequest, BindingResult bindingResult) throws SystemException {
         if (bindingResult.hasErrors()) throw new BlankFieldException("Необходимо заполнить все поля");
         return modelMapper.map(humanService.signUp(signUpRequest), HumanDTO.class);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("sign-in")
+    public String signIn(CsrfToken csrfToken) {
+        return csrfToken.getToken();
     }
 }
